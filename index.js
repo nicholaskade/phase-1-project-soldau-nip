ed3af994135571d7e31407511361048862887714
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-14 15:02:46 -0500

    Added dropdown menus, sorted by state

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('https://ttp.cbp.dhs.gov/schedulerapi/locations/?temporary=false&inviteOnly=false&operational=true&serviceName=Global%20Entry')
     .then(response => response.json())
     .then(locationData => {
-        generateDropDownOptions(locationData)
-        console.log(locationData)
+        pullData(locationData)
+        // console.log(locationData)
     })
 })
 
 let locationNames = []
 let locationNamesWithIds = []
 let validStates = []
+let stateSorter;
 
-function generateDropDownOptions(locationData){
+function pullData(locationData){
     locationData.forEach((location) => {
         if (location.shortName !== "Permanently Closed") {
             if (location.state !== "" && location.countryCode === "US") {
@@ -26,22 +27,43 @@ function generateDropDownOptions(locationData){
         locationId: `${location.id}`
         })
     })
-    locationNames.sort()
-    locationNames.forEach((location) => {
-        const locationSelector = document.getElementById('location-selector')
-        const generateOptions = document.createElement('option')
-        generateOptions.innerText = location
-        locationSelector.appendChild(generateOptions)
-    })
+    // sorts alphabetically
     validStates.sort()
+    renderStates()
+}
+
+function renderStates() {
+    console.log(validStates)
     validStates.forEach((state) => {
+        // console.log(state)
         const stateSelector = document.getElementById('state-selector')
-        const generateOptions = document.createElement('option')
+        const generateOptions = document.createElement('button')
         generateOptions.innerText = state
+        generateOptions.className = "dropdown-item"
+        // event listener for when a state is selected
+        generateOptions.addEventListener('click', (e) =>{
+            e.preventDefault()
+            stateSorter = e.target.innerText
+            filterLocationsByState(stateSorter)        
+        })
         stateSelector.appendChild(generateOptions)
     })
-    const stateSelector = document.getElementById('state-selector')
-    stateSelector.onchange = () => {
-        alert("You selected a state!")
+}
+
+function filterLocationsByState(stateSorter) {
+// sorts alphabetically
+    const locationSelector = document.getElementById('location-selector')
+    while (locationSelector.firstChild) {
+        locationSelector.removeChild(locationSelector.firstChild)
     }
+    locationNames.sort()
+    locationNames.forEach((location) => {
+        if ((`${location[0] + location[1]}`) === stateSorter) {
+            const generateOptions = document.createElement('button')
+            generateOptions.innerText = location
+            generateOptions.className = "dropdown-item"
+            // grab the state abbrevs. and make them the IDs of each location so we can filter with them
+            locationSelector.appendChild(generateOptions)
+        }
+    })
 }
